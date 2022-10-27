c87cd9eba68f926c5b2a33fcfe318033de3d4cf8
Sherifwaelnagaty <Sherifwael0@gmail.com>
2022-10-27 16:09:56 +0200

    Adding Login Screen

---
+++ lib/views/login_screen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:grad_project/DefaultButton.dart';
+
+class LoginScreen extends StatefulWidget {
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+  var formkey =GlobalKey<FormState>();
+  bool isPassword=true;
+@override
+  void setState(VoidCallback fn) {
+    super.setState(fn);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Form(
+              key: formkey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Login",
+                    style:TextStyle(
+                      fontSize: 40,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+              defaultFormField(
+                prefix: Icons.email,
+                validate: (value) {
+                  if (value!.isEmpty) {
+                    return "Email must not be empty";
+                  }
+                  return null;
+                },
+                Label:"Email" ,
+                type: TextInputType.visiblePassword,
+                Controller: emailController,
+              ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  defaultFormField(
+                    suffix: isPassword?Icons.visibility:Icons.visibility_off,
+                    icon: (){
+                      setState((){
+                        isPassword=!isPassword;
+                      });
+                    },
+                    prefix: Icons.lock,
+                    validate: (value) {
+                      if (value!.isEmpty) {
+                        return "Password must not be empty";
+                      }
+                      return null;
+                    },
+                    Label:"Password" ,
+                    type: TextInputType.visiblePassword,
+                    Controller: passwordController,
+                    isPassword: isPassword,
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  defaultButton(
+                    text: 'login',
+                    function: (){
+                      if(formkey.currentState!.validate()){
+                        print(emailController.text);
+                        print(passwordController.text);
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                          "Don\'t have an account?",
+                      ),
+                      TextButton(
+                        onPressed: () {  },
+                        child: Text(
+                            "Register",
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
