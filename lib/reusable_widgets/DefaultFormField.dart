42d232c436cf82be1c1aca2da47d5742ca2033aa
Sherifwaelnagaty <Sherifwael0@gmail.com>
2022-10-27 16:47:31 +0200

    Edit DefaultFormField class

--- lib/reusable_widgets/DefaultFormField.dart
+++ lib/reusable_widgets/DefaultFormField.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 class DefaultFormField extends StatelessWidget {
-  const DefaultFormField({Key? key,
+   DefaultFormField({Key? key,
     required this.type,
-    this.width = double.infinity,
     required this.Controller,
-    this.background=Colors.blue,
+    required this.Label,
+    required this.validate,
+    required this.prefix,
+    this.isPassword=false,
+    this.icon,
+    this.suffix,
   }) : super(key: key);
   TextEditingController Controller;
   TextInputType type;
   String Label;
-  Color background;
   var validate;
   IconData prefix;
   var suffix;
@@ -19,35 +22,25 @@ class DefaultFormField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextFormField(
+      validator:
+      validate,
+      controller:Controller,
+      keyboardType: type,
+      obscureText: isPassword,
+      decoration: InputDecoration(
+        labelText: "${Label}",
+        prefixIcon: Icon(
+            prefix
+        ),
+        suffixIcon: suffix!= null? IconButton(
+          onPressed: icon,
+          icon: Icon(
+              suffix
+          ),
+        ):null,
+        border:OutlineInputBorder(),
+      ),
+    );
   }
 }
-Widget defaultFormField({
-  required TextEditingController Controller,
-  required TextInputType type,
-  required String Label,
-  required var validate,
-  required IconData prefix,
-  var suffix,
-  bool isPassword=false,
-  var icon,
-})=>TextFormField(
-  validator:
-  validate,
-  controller:Controller,
-  keyboardType: type,
-  obscureText: isPassword,
-  decoration: InputDecoration(
-    labelText: "${Label}",
-    prefixIcon: Icon(
-        prefix
-    ),
-    suffixIcon: suffix!= null? IconButton(
-      onPressed: icon,
-      icon: Icon(
-          suffix
-      ),
-    ):null,
-    border:OutlineInputBorder(),
-  ),
-);
