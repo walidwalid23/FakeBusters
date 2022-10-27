f7206cecd4ac0e87ce2f676aff1ec4971d998711
Walid <walid.tawfik200@gmail.com>
2022-10-27 17:05:15 +0200

    added text style and container shadows and radius

--- lib/reusable_widgets/user_choice.dart
+++ lib/reusable_widgets/user_choice.dart
@@ -25,7 +25,33 @@ class UserChoice extends StatelessWidget {
             CircleAvatar(child: Image.asset(imagePath,scale:imageScale ),
                 radius:circleRadius,
             backgroundColor: ColorsManager.choiceCircleBackground,),
-            Text(choiceText)
+            Container(
+              padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  border:  Border.all(
+                    color: Colors.orange,
+                    width: 3,
+                  ), //Border.all
+                    borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      offset: const Offset(
+                        3.0,
+                        3.0,
+                      ), //Offset
+                      blurRadius: 10.0,
+                      spreadRadius: 1.0,
+                    ), //BoxShadow
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: const Offset(0.0, 0.0),
+                      blurRadius: 0.0,
+                      spreadRadius: 0.0,
+                    ), //BoxShadow
+                  ],
+                ),
+                child: Text(choiceText))
 
           ],
         ),
