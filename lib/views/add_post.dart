ea7550b60bce2ab8757b7ce9d1c0ca5a6923f03d
Walid <walid.tawfik200@gmail.com>
2022-10-27 23:06:27 +0200

    initial add post form

--- lib/views/add_post.dart
+++ lib/views/add_post.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
 class  AddPost extends StatelessWidget {
-  const AddPost({Key? key}) : super(key: key);
-
+    AddPost({Key? key}) : super(key: key);
+    
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body:Center(child:Text("AddPost")));
+    return  Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+
+
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
