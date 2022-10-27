6b21248cc43a4e19f670155f0751305e64a67a29
Walid <walid.tawfik200@gmail.com>
2022-10-27 19:22:06 +0200

    Drawer Ui modified

--- lib/models/styles_manager.dart
+++ lib/models/styles_manager.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class StylesManager{
 
   static TextStyle textStyle1 = TextStyle(fontWeight: FontWeight.bold);
+  static TextStyle notificationStyle = TextStyle(fontWeight: FontWeight.bold,
+  color:Colors.red);
 
 }
 
