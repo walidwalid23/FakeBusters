3ff46f4bf21e347937da1047428db4af4cfa469b
Sherifwaelnagaty <Sherifwael0@gmail.com>
2022-10-28 16:27:50 +0200

    Adding NewStyle

--- lib/models/styles_manager.dart
+++ lib/models/styles_manager.dart
@@ -5,6 +5,11 @@ class StylesManager{
   static TextStyle textStyle1 = TextStyle(fontWeight: FontWeight.bold);
   static TextStyle notificationStyle = TextStyle(fontWeight: FontWeight.bold,
   color:Colors.red);
+  static TextStyle textStyle2 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 18,
+  );
+
 
 
 
