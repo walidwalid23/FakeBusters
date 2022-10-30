11a8b4d89081e81e3c0cfabf906c5d2e04c4a69c
Sherifwaelnagaty <Sherifwael0@gmail.com>
2022-10-30 18:04:05 +0200

    Eidting Hint Screen

--- lib/views/hint.dart
+++ lib/views/hint.dart
@@ -5,6 +5,11 @@ class HintScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> str = [
+      "Hello World",
+      "This is FlutterCampus",
+      "Learn App Building.",
+      "Flutter is the Best"];
     return Scaffold(
       appBar: AppBar(
         title: Center(
@@ -18,9 +23,39 @@ class HintScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            SizedBox(
+              height: 10,
+            ),
             Text(
-                'How to know whether it\'s fake or not',
+                'How to know whether it\'s fake or not?',
               style: StylesManager.textStyle2,
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                  children:[
+                    Text(
+                      "\u2022",
+                      style: TextStyle(
+                          fontSize: 16
+                      ),
+                    ), //bullet text
+                    SizedBox(
+                      width: 10,
+                    ), //space between bullet and text
+                    Expanded(
+                      child:Text(
+                        "Check the Logo Dimensions",
+                        style: TextStyle(
+                            fontSize: 16
+                        ),
+                      ), //text
+                    ),
+                  ],
+              ),
             ),
           ],
         ),
