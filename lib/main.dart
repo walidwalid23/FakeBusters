e8688630e4b8d487746d253a3e7cb1b246d781e2
Sherifwaelnagaty <Sherifwael0@gmail.com>
2022-10-30 17:43:18 +0200

    Adding Hint Screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:fakebustersapp/models/styles_manager.dart';
 import 'package:fakebustersapp/models/theme_manager.dart';
 import 'package:fakebustersapp/views/Profile.dart';
 import 'package:fakebustersapp/views/edit_post.dart';
+import 'package:fakebustersapp/views/hint.dart';
 import 'package:fakebustersapp/views/upload_post.dart';
 import 'package:fakebustersapp/views/categories.dart';
 import 'package:fakebustersapp/views/display_post.dart';
@@ -28,19 +29,20 @@ class MyApp extends StatelessWidget {
         appBarTheme: AppThemeManager.appBarTheme,
       ),
       title: 'FakeBusters',
-      routes: {'/home':(context)=> Home(),
-        '/profile':(context)=> ProfileScreen(),
-        '/settings':(context)=> Settings(),
-        '/search':(context)=> Search(),
-        '/notifications':(context)=> NotificationsScreen(),
-        '/signup':(context)=> SignupScreen(),
-        '/login':(context)=> LoginScreen(),
-        '/categories':(context)=> Categories(),
-        '/uploadpost':(context)=> UploadPost(),
-        '/displaypost':(context)=> DisplayPost(),
-        '/editpost':(context)=>EditPost(),
+      routes: {'/home':(context)=> Home(), //Waleed
+        '/profile':(context)=> ProfileScreen(),//Sherif
+        '/settings':(context)=> Settings(),//Abdelrahman
+        '/search':(context)=> Search(),//Adham
+        '/notifications':(context)=> NotificationsScreen(),//Waleed
+        '/signup':(context)=> SignupScreen(),//Abdelrahman
+        '/login':(context)=> LoginScreen(),//Sherif
+        '/categories':(context)=> Categories(),//Waleed
+        '/uploadpost':(context)=> UploadPost(),//Waleed
+        '/displaypost':(context)=> DisplayPost(),//Waleed
+        '/editpost':(context)=>EditPost(),//Sherif
+        '/hint':(context)=>HintScreen(),//Sherif
       },
-      initialRoute:'/home' ,
+      initialRoute:'/hint' ,
     );
   }
 }
