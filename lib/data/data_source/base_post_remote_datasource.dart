110b4e61bcd91af30639049ff078323ac0a281ee
AdhamElsherbiny <97008942+AdhamElsherbiny@users.noreply.github.com>
2023-01-06 18:16:44 +0200

    implemented findByPostsByProductName

--- lib/data/data_source/base_post_remote_datasource.dart
+++ lib/data/data_source/base_post_remote_datasource.dart
@@ -3,14 +3,15 @@ import 'package:fakebustersapp/domain/entities/post.dart';
 import '../../core/exception_handling/success.dart';
 import '../../domain/entities/uploaded_post.dart';
 import '../../domain/entities/vote.dart';
-abstract class BasePostRemoteDataSource{
+
+abstract class BasePostRemoteDataSource {
   Future<UploadingPostSuccess> uploadPost(UploadedPost post, String userToken);
-  Future<List<Post>> findPostsByCategories(List<String> categories, String userToken);
+  Future<List<Post>> findPostsByCategories(
+      List<String> categories, String userToken);
+  Future<List<Post>> findPostsByProductName(
+      String productName, String userToken);
   Future<Vote> incrementFakeVotes(String postID, String userToken);
   Future<Vote> incrementOriginalVotes(String postID, String userToken);
   Future<Vote> getPostVotes(String postID, String userToken);
   Future<String> deletePost(String postID, String userToken);
 }
-
-
-
--- lib/domain/usecases/search_posts_by_product_name_usecase.dart
+++ lib/domain/usecases/search_posts_by_product_name_usecase.dart
@@ -1,1 +1,14 @@
-class SearchPostsByProductNameUseCase {}
+import 'package:fakebustersapp/domain/entities/post.dart';
+import 'package:dartz/dartz.dart';
+import 'package:fakebustersapp/core/exception_handling/failures.dart';
+import 'package:fakebustersapp/domain/domain_repository/base_post_repository.dart';
+
+class SearchPostsByProductNameUseCase {
+  BasePostRepository postRepository;
+  SearchPostsByProductNameUseCase(this.postRepository);
+
+  Future<Either<Failure, List<Post>>> excute(
+      String productName, String userToken) async {
+    return await postRepository.findPostsByProductName(productName, userToken);
+  }
+}
