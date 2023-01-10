3eb433c0c909d616f8d5221068639df95628eae1
abdelrahman hesham <abdelrahman1902703@miuegypt.edu.eg>
2023-01-10 06:25:12 +0200

    theme color change

--- lib/core/utils/constants/theme_manager.dart
+++ lib/core/utils/constants/theme_manager.dart
@@ -3,24 +3,27 @@ import 'colors_manager.dart';
 
 class AppThemeManager {
   static final ThemeData darkMode = ThemeData(
-    splashColor: ColorsManager.themeColor1,
-    secondaryHeaderColor: Color.fromARGB(255, 94, 94, 94),
-    primaryColor: Colors.grey[700],
-    scaffoldBackgroundColor: Color.fromARGB(255, 85, 84, 84),
-    colorScheme:
-        ColorScheme.light(primary: Colors.white, secondary: Color(0xFFF3A953)),
-    iconTheme: IconThemeData(
-      color: Colors.white,
-    ),
-    appBarTheme: AppBarTheme(backgroundColor: Color.fromARGB(255, 85, 84, 84)),
-    textTheme: TextTheme(
-      bodyText1: TextStyle(),
-      bodyText2: TextStyle(),
-    ).apply(
-      bodyColor: Colors.white,
-      displayColor: Colors.white,
-    ),
-  );
+      splashColor: ColorsManager.themeColor1,
+      secondaryHeaderColor: Color.fromARGB(255, 94, 94, 94),
+      primaryColor: Colors.grey[700],
+      scaffoldBackgroundColor: Color.fromARGB(255, 85, 84, 84),
+      colorScheme: ColorScheme.light(
+          primary: Colors.white, secondary: Color(0xFFF3A953)),
+      iconTheme: IconThemeData(
+        color: Colors.white,
+      ),
+      appBarTheme:
+          AppBarTheme(backgroundColor: Color.fromARGB(255, 85, 84, 84)),
+      textTheme: TextTheme(
+        bodyText1: TextStyle(),
+        bodyText2: TextStyle(),
+      ).apply(
+        bodyColor: Colors.white,
+        displayColor: Colors.white,
+      ),
+      drawerTheme: DrawerThemeData(
+        backgroundColor: Color.fromARGB(255, 85, 84, 84),
+      ));
 
   static final ThemeData lightMode = ThemeData(
     splashColor: Color.fromARGB(255, 255, 0, 0),
