af33ad01521a9a1b2664d68b6c6692de00503834
abdelrahman hesham <abdelrahman1902703@miuegypt.edu.eg>
2023-01-10 20:10:00 +0200

    add post color edit

--- lib/presentation/reusable_widgets/add_post_form_field.dart
+++ lib/presentation/reusable_widgets/add_post_form_field.dart
@@ -1,33 +1,37 @@
 import 'package:fakebustersapp/core/utils/constants/colors_manager.dart';
 import 'package:flutter/material.dart';
-class AddPostTextInputField  extends StatelessWidget {
-    AddPostTextInputField ({Key? key, required this.label, required this.validatorFunc,
-    required this.textFieldController}) : super(key: key);
+
+class AddPostTextInputField extends StatelessWidget {
+  AddPostTextInputField(
+      {Key? key,
+      required this.label,
+      required this.validatorFunc,
+      required this.textFieldController})
+      : super(key: key);
   String label;
   String? Function(String? value) validatorFunc;
   TextEditingController textFieldController;
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
         padding: const EdgeInsets.all(15),
-    child:TextFormField(
-      controller:textFieldController,
-      maxLength: 60,
-      decoration: InputDecoration(
-        labelText: label,
-        labelStyle: TextStyle(color:Colors.grey),
-        border: OutlineInputBorder(
-          borderSide: BorderSide(color:Colors.black,),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color:ColorsManager.themeColor1!),
-        ),
-
-      ),
-
-      validator: validatorFunc,
-    )
-    );
+        child: TextFormField(
+          controller: textFieldController,
+          maxLength: 60,
+          decoration: InputDecoration(
+            labelText: label,
+            labelStyle: TextStyle(color: ColorsManager.themeColor1),
+            border: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.black,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: ColorsManager.themeColor1!),
+            ),
+          ),
+          validator: validatorFunc,
+        ));
   }
 }
