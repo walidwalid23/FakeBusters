d2f3982b40523397a162e0793429927027c19d75
abdelrahman hesham <abdelrahman1902703@miuegypt.edu.eg>
2023-01-10 21:24:07 +0200

    theme change

--- lib/core/utils/constants/theme_manager.dart
+++ lib/core/utils/constants/theme_manager.dart
@@ -3,27 +3,28 @@ import 'colors_manager.dart';
 
 class AppThemeManager {
   static final ThemeData darkMode = ThemeData(
-      splashColor: ColorsManager.themeColor1,
-      secondaryHeaderColor: Color.fromARGB(255, 94, 94, 94),
-      primaryColor: Colors.grey[700],
-      scaffoldBackgroundColor: Color.fromARGB(255, 85, 84, 84),
-      colorScheme: ColorScheme.light(
-          primary: Colors.white, secondary: Color(0xFFF3A953)),
-      iconTheme: IconThemeData(
-        color: Colors.white,
-      ),
-      appBarTheme:
-          AppBarTheme(backgroundColor: Color.fromARGB(255, 85, 84, 84)),
-      textTheme: TextTheme(
-        bodyText1: TextStyle(color: Colors.deepOrange),
-        bodyText2: TextStyle(color: Colors.deepOrange),
-      ).apply(
-        bodyColor: Colors.deepOrange,
-        displayColor: Colors.deepOrange,
-      ),
-      drawerTheme: DrawerThemeData(
-        backgroundColor: Color.fromARGB(255, 85, 84, 84),
-      ));
+    splashColor: ColorsManager.themeColor1,
+    secondaryHeaderColor: Color.fromARGB(255, 94, 94, 94),
+    primaryColor: Colors.grey[700],
+    scaffoldBackgroundColor: Color.fromARGB(255, 85, 84, 84),
+    colorScheme:
+        ColorScheme.light(primary: Colors.white, secondary: Color(0xFFF3A953)),
+    iconTheme: IconThemeData(
+      color: Colors.white,
+    ),
+    appBarTheme: AppBarTheme(backgroundColor: Color.fromARGB(255, 85, 84, 84)),
+    textTheme: TextTheme(
+      bodyText1: TextStyle(color: Colors.deepOrange),
+      bodyText2: TextStyle(color: Colors.deepOrange),
+    ).apply(
+      bodyColor: Colors.deepOrange,
+      displayColor: Colors.deepOrange,
+    ),
+    drawerTheme: DrawerThemeData(
+      backgroundColor: Color.fromARGB(255, 85, 84, 84),
+      
+    ),    
+  );
 
   static final ThemeData lightMode = ThemeData(
     splashColor: Color.fromARGB(255, 255, 0, 0),
