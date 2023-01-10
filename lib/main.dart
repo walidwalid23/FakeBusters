5973b3343e8311b0e4d5dab0d07d1edfc5804115
abdelrahman hesham <abdelrahman1902703@miuegypt.edu.eg>
2023-01-10 05:44:19 +0200

    main theme change

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,29 @@
 import 'package:fakebustersapp/core/utils/constants/theme_manager.dart';
+import 'package:fakebustersapp/presentation/controller/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'core/routing/gorouter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-
 void main() {
   runApp(ProviderScope(child:MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
     MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
-      theme: ThemeData(
-        appBarTheme: AppThemeManager.appBarTheme,
-      ),
-      title: 'FakeBusters',
-    );
+      theme:ref.watch(themeProvider).when(            
+            data: (data)=> (data=="light")?AppThemeManager.lightMode:AppThemeManager.darkMode ,
+            error: (err, st)=>AppThemeManager.lightMode,
+            loading: ()=> AppThemeManager.lightMode)
+             
+    ,title: 'FakeBusters',);
+    
+
+    
+    
   }
-
-
 }
